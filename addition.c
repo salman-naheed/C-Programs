64d724416e14162a97dea0744a0d6f6551b2e8d9
salman-naheed <salmannaheed23@gmail.com>
2021-06-27 02:15:23 +0500

    Renamed some files

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("Enter no. a: \n ");
+    scanf("%d",&a);
+
+    printf("Enter no. b: \n ");
+    scanf("%d",&b);
+
+    printf("Sum is: %d ", a+b);
+    return 0;
+}
